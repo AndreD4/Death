ea85047dc0911943318ef5a4296f202205e5da18
AndreD4 <andremai629@gmail.com>
2023-06-06 15:13:33 +0900

    Enemy AI- attack if provoked

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -10,6 +10,7 @@ public class Enemy_AI : MonoBehaviour
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     
     void Start()
     {
@@ -25,11 +26,38 @@ public class Enemy_AI : MonoBehaviour
     void SetDestionation()
     {
       distanceToTarget = Vector3.Distance(target.position, transform.position);
-
-      if(distanceToTarget <= chaseRange)
+      if(isProvoked)
       {
-      navMeshAgent.SetDestination(target.position);
+        EngagedTarget();
+      }
+      else if(distanceToTarget <= chaseRange)
+      {
+        isProvoked = true;
+
+        
       }    
+    }
+
+    private void EngagedTarget()
+    {
+      if(distanceToTarget >= navMeshAgent.stoppingDistance)
+      {
+        ChaseTarget();
+      }
+      if (distanceToTarget <= navMeshAgent.stoppingDistance)
+      {
+        AttackTarget();
+      }
+    }
+
+    private void ChaseTarget()
+    {
+      navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + "has got yah" + target.name);
     }
 
     void OnDrawGizmosSelected() 
