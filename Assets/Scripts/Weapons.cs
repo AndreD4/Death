53eae2057ff5626aebf59f7b91bd3cc17e1fdf43
AndreD4 <andremai629@gmail.com>
2023-06-07 10:24:43 +0900

    printing to console Raycasting

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire"))
+          {
+            Shoot();
+          }
+    }
+
+    private void Shoot()
+    {
+      RaycastHit hit;
+      Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+      Debug.Log("i hit something" + hit.transform.name);
+    }
+}
