0d065e45329451a6b47f549517b6038e274a20e7
AndreD4 <andremai629@gmail.com>
2023-06-07 10:47:03 +0900

    damage method

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,7 +6,7 @@ public class EnemyHealth : MonoBehaviour
 {
   [SerializeField] float hitPoints =100f;
 
-  public void TakeDamage(float damage)
+  public void TakeDamage (float damage)
   {
     hitPoints -= damage;
     if(hitPoints <=0)
