21034a0a4335598aa1ca474f42de5fbcdc71e802
AndreD4 <andremai629@gmail.com>
2023-06-12 10:56:03 +0900

    Creating new loop and quit method for buttons

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ReloadGame()
     {
-        
+      SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+      Application.Quit();
     }
 }
